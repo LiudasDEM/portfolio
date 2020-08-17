55b8956a6f60adef88024be8388855b5edd86462
Liudas Demikis <liudas.demikis@zenitech.co.uk>
2020-08-17 19:57:24 +0300

    add initial navigation

--- admin/src/components/Layout.jsx
+++ admin/src/components/Layout.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
-import { Fade, Container } from 'react-bootstrap'
+import { Route, Link } from 'react-router-dom'
+import { Fade, Container, Row, Col, ListGroup } from 'react-bootstrap'
+
+
+import { useAuth } from '../contexts/Auth'
 
 
 import { UsersList, UsersEdit } from './Users'
@@ -8,12 +11,34 @@ import { UserGroupsList, UserGroupsEdit } from './UserGroups'
 
 
 function Layout() {
+	const { user } = useAuth()
+
 	return <Fade in>
-		<Container>
-			<Route path="/users" exact component={UsersList} />
-			<Route path="/users/:id" component={UsersEdit} />
-			<Route path="/user-groups" exact component={UserGroupsList} />
-			<Route path="/user-groups/:id" component={UserGroupsEdit} />
+		<Container fluid="xl">
+			<Row style={{ marginTop: '20px' }}>
+				<Col><Link to={'/'}>Home</Link></Col>
+				<Col md={{ span: 4, offset: 4 }}>{user.email}</Col>
+			</Row>
+			<Row style={{ marginTop: '20px' }}>
+				<Col md={3}>
+					<ListGroup>
+						<ListGroup.Item action variant="light">
+							<Link to={'/users'}>Users</Link>
+						</ListGroup.Item>
+						<ListGroup.Item action variant="light">
+							<Link to={'/user-groups'}>User groups</Link>
+						</ListGroup.Item>
+					</ListGroup>
+				</Col>
+				<Col md={8} variant="light">
+					<Container fluid>
+						<Route path="/users" exact component={UsersList} />
+						<Route path="/users/:id" component={UsersEdit} />
+						<Route path="/user-groups" exact component={UserGroupsList} />
+						<Route path="/user-groups/:id" component={UserGroupsEdit} />
+					</Container>
+				</Col>
+			</Row>
 		</Container>
 	</Fade>
 }
